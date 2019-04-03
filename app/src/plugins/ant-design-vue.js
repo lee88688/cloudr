e4bfcc017789db8490ce42382788a51e0d8f80b6
李思 <lisi@cmiot.chinamobile.com>
2019-04-03 17:23:04 +0800

    添加web app 基本框架

---
+++ app/src/plugins/ant-design-vue.js
@@ -0,0 +1,5 @@
+import 'ant-design-vue/dist/antd.css'
+import antd from 'ant-design-vue'
+import Vue from 'vue'
+
+Vue.use(antd)
--- app/src/views/index/Index.vue
+++ app/src/views/index/Index.vue
@@ -1,13 +1,57 @@
 <template>
-    $END$
+  <a-layout style="height: 100%;">
+    <a-layout-header class="header-bar">
+      <div class="brand">
+        <a-icon type="cloud" /><span>cloudr</span>
+      </div>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu mode="inline">
+          <a-menu-item>
+            <a-icon type="folder" />
+            <span>全部文件</span>
+          </a-menu-item>
+          <a-menu-item>
+            <span>图片</span>
+          </a-menu-item>
+          <a-menu-item>
+            <span>文档</span>
+          </a-menu-item>
+          <a-menu-item>
+            <span>视频</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-content>
+          content
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
-  export default {
-    name: 'Index'
-  }
+export default {
+  name: 'Index'
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.header-bar {
+  color: #fff;
+  display: flex;
+  justify-content: start;
+  align-content: center;
 
+  .brand {
+    font-size: 20px;
+    cursor: pointer;
+
+    > span {
+      margin-left: 10px;
+    }
+  }
+}
 </style>
